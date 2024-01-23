705e6e271d9584ebbec61f59f57e0ded8e1cdb28
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-23 21:59:14 +0300

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,46 @@
-const copy = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+
+const exists = async (path) => {
+    try {
+        await fs.access(path);
+        return true;
+    } catch {
+        return false;
+    }
 };
 
+const urlOrign = 'src/fs/files';
+const urlCopy = 'src/fs/files_copy';
+// Получение абсолютного пути
+const absolutePathOrign = path.resolve(urlOrign);
+const absolutePathCopy = path.resolve(urlCopy);
+
+const copy = async () => {
+    // Write your code here
+    try {
+        if (
+            (await exists(absolutePathCopy)) ||
+            !(await exists(absolutePathOrign))
+        ) {
+            throw new Error('FS operation failed');
+        }
+
+        //Получаем файлы из папки files
+        const files = await fs.readdir(absolutePathOrign);
+
+        //создаем папку files_copy
+        await fs.mkdir(absolutePathCopy);
+
+        const promises = files.map((fileName) => {
+            fs.copyFile(
+                path.join(absolutePathOrign, fileName),
+                path.join(absolutePathCopy, fileName)
+            );
+        });
+        await Promise.all(promises);
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 await copy();
