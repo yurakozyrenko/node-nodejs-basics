aaaea55c6274ae159e6a0a63565e24c05c74e943
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 09:44:43 +0300

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const url = 'src/fs/files';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const content = await fs.readdir(absolutePath);
+        console.log(content);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
