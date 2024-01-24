6d759905a4089a75d3ce983178c9b3c5e297cf60
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 19:41:34 +0300

    Update read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { pipeline } from 'node:stream/promises';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+
+const url = 'src/streams/files';
+const fileName = 'fileToRead.txt';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    await pipeline(createReadStream(filePath), process.stdout);
 };
 
 await read();
