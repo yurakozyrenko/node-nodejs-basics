e453609addd5362c5aea584616a1082813e4c08f
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 21:35:46 +0300

    Update transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+const dataRevers = (data) => data.toString().split('').reverse().join('');
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const myTransform = new Transform({
+        transform(data, encoding, callback) {
+            callback(null, dataRevers(data));
+        },
+    });
+
+    await pipeline(process.stdin, myTransform, process.stdout);
 };
 
 await transform();
