df64c5cab40c4e890a5bf430ed3a8dfa68b4c17a
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 14:32:48 +0300

    Update env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+const PREF = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const arrayEnv = Object.entries(process.env).reduce((acc, [key, value]) => {
+        if (key.startsWith(PREF)) {
+            acc.push(key + '=' + value);
+            return acc;
+        }
+        return acc;
+    }, []);
+    const resultString = arrayEnv.join('; ');
+    console.log(resultString);
 };
 
 parseEnv();
