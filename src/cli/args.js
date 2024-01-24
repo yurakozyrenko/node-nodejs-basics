6f3b8fc3b4c76955a84b5cc10e04b541c8406bb4
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 13:54:37 +0300

    Update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+const PREF = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const arrayArgs = process.argv.reduce((acc, element, index, array) => {
+        if (element.startsWith(PREF)) {
+            acc.push(element.replace(PREF, '') + ' is ' + array[index + 1]);
+            return acc;
+        }
+        return acc;
+    }, []);
+    const resultString = arrayArgs.join(', ');
+    console.log(resultString);
 };
 
 parseArgs();
