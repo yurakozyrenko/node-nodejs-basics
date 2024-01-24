a786882e282885f01a264a1b3bac8370fd962fc3
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 23:42:40 +0300

    Update compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+
+const gzip = createGzip();
+
+const url = 'src/zip/files';
+const fileName = 'fileToCompress.txt';
+const fileNameCompress = 'archive.gz';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+const filePathZip = path.join(absolutePath, fileNameCompress);
+
+const source = createReadStream(filePath);
+const destination = createWriteStream(filePathZip);
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    await pipeline(source, gzip, destination);
 };
 
 await compress();
