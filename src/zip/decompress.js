3a58e8c9f305d20a1551c30b03781b2593403a00
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 23:43:02 +0300

    Update decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+
+const gzip = createGunzip();
+
+const url = 'src/zip/files';
+const fileName = 'fileToCompress.txt';
+const fileNameDecompress = 'archive.gz';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+const filePathUnzip = path.join(absolutePath, fileNameDecompress);
+
+const source = createReadStream(filePathUnzip);
+const destination = createWriteStream(filePath);
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+
+    await pipeline(source, gzip, destination);
 };
 
 await decompress();
