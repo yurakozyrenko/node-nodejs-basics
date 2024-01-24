f62418d98593a1a3092e26a0e20304870e2cd1f1
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 09:41:07 +0300

    Update delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
-const remove = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises';
+import path from 'path';
 
+const url = 'src/fs/files';
+const fileName = 'fileToRemove.txt';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+
+const remove = async () => {
+    // Write your code here
+    try {
+        // Удаляем файл, проверка не нужна
+        await fs.unlink(filePath);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+};
 await remove();
