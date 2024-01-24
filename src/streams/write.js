62f034e1265a9a1ddf606a44ee5830c6b06c3d8e
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 20:45:36 +0300

    Update write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { pipeline } from 'node:stream/promises';
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+
+const url = 'src/streams/files';
+const fileName = 'fileToWrite.txt';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    await pipeline(process.stdin, createWriteStream(filePath));
 };
 
 await write();
