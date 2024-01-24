a56eb8dcd32713d77e235ce2532ef801203ea54b
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 09:56:52 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+
+const url = 'src/fs/files';
+const fileName = 'fileToRead.txt';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const content = await fs.readFile(filePath, 'utf8');
+        console.log(content);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
