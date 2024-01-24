b430670cb339bc326db91085172e0de2a4843e9f
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 17:05:24 +0300

    Update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import crypto from 'node:crypto';
+
+const url = 'src/hash/files';
+const fileName = 'fileToCalculateHashFor.txt';
+// Получение абсолютного пути
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const content = await fs.readFile(filePath);
+    const value = crypto.createHash('SHA256').update(content);
+    const resultValue = value.digest('hex');
+    console.log(resultValue);
 };
 
 await calculateHash();
