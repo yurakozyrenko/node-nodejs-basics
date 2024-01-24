1ade53a6c4f08466986ab53ec696368a6432fed3
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-01-24 09:38:08 +0300

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const exists = async (path) => {
+    try {
+        await fs.access(path);
+        return true;
+    } catch {
+        return false;
+    }
+};
+const url = 'src/fs/files';
+const fileName = 'wrongFilename.txt';
+const newFileName = 'properFilename.md';
+ 
+// Получение абсолютного пути файлов
+const absolutePath = path.resolve(url);
+const filePath = path.join(absolutePath, fileName);
+const newFilePath = path.join(absolutePath, newFileName);
+
 const rename = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        if (await exists(newFilePath)) {
+            throw new Error('FS operation failed');
+        }
+        // Переименовывает файл
+        await fs.rename(filePath, newFilePath);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 await rename();
